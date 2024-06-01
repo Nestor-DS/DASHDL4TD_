023d2dc947e46770ae819610510474cefb6235b5
Nestor-DS <nestorduhamel18@outlook.es>
2024-06-01 12:41:14 -0600

    Design Update

--- pages/show_table.py
+++ pages/show_table.py
@@ -8,36 +8,38 @@ data_path = "./data/drinking_water_potability.csv"
 df = pd.read_csv(data_path)
 
 # Design the layout to show the interactive table
-layout = html.Div([
-    html.Link(
-        rel='stylesheet',
-        href='/assets/styles.css'  # Ruta al archivo CSS que has creado
-    ),
-    html.H1("Contenido del Archivo CSV"),
-    
-    html.P(
-        "En la siguiente tabla se muestra el contenido del archivo CSV cargado:", 
-        style={'marginBottom': '20px', 'fontSize': '1.2em', 'textAlign': 'center'}),
-    
-    dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": col, "id": col} for col in df.columns
-        ],
-        data=df.to_dict('records'),
-        sort_action='native',
-        page_size=10,
-        filter_action="native",
-        style_table={'overflowX': 'auto'},
-        style_header={
-            'backgroundColor': 'rgb(230, 230, 230)',
-            'fontWeight': 'bold'
-        },
-        style_cell={
-            'whiteSpace': 'normal',
-            'height': 'auto',
-            'textAlign': 'left',
-            'minWidth': '180px', 'width': '180px', 'maxWidth': '180px'
-        }
-    )
-])
+layout = html.Div(
+    [
+        html.Link(
+            rel='stylesheet',
+            href='/assets/styles.css'  # Ruta al archivo CSS que has creado
+        ),
+        html.H1("Contenido del Archivo CSV"),
+        html.P(
+            "Este proyecto se centra en la aplicación de métodos de aprendizaje profundo a conjuntos de datos tabulares relacionados con el fin de encontrar los mejores resultados posibles. Se utilizan diferentes enfoques para la exploración de datos, la clasificación con bosques aleatorios y técnicas de aprendizaje profundo.",
+            style={'marginBottom': '20px', 'fontSize': '1.5em', 'textAlign': 'lift'}
+        ),
+        dash_table.DataTable(
+            id='datatable-interactivity',
+            columns=[{"name": col, "id": col} for col in df.columns],
+            data=df.to_dict('records'),
+            sort_action='native',
+            page_size=10,
+            filter_action="native",
+            style_table={'overflowX': 'auto'},
+            style_header={
+                'backgroundColor': 'rgb(230, 230, 230)',
+                'fontWeight': 'bold'
+            },
+            style_cell={
+                'whiteSpace': 'normal',
+                'height': 'auto',
+                'textAlign': 'left',
+                'minWidth': '180px',
+                'width': '180px',
+                'maxWidth': '180px'
+            }
+        )
+    ],
+    style={'margin': '20px'}
+)
